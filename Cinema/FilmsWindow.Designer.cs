a171bcacab5cc2aacc6ebfefd4e9deb87f201ac3
Teacher <softmasterlab@hotmail.com>
2020-03-10 09:57:34 +0200

    first commit

---
+++ Cinema/FilmsWindow.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Cinema
+{
+    partial class FilmsWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FilmsWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 381);
+            this.Name = "FilmsWindow";
+            this.Text = "FilmsWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
